01ae69c0ebf4091f93a9edcabba293f2bf034ce8
karinareiswich <karinareiswich@yandex.ru>
2022-09-06 19:51:16 +0300

    adaptive

---
+++ index.js
@@ -0,0 +1,9 @@
+/*Mobile menu*/
+
+function openModal() {
+    document.getElementById("mobile").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("mobile").style.top = "-800px";
+}
